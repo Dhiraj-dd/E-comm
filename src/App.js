03e0dce500c891e3afcb481c2e07b8b78ace768a
Dheeraj Deshmukh <122549392+Dhiraj-dd@users.noreply.github.com>
2023-08-18 12:56:34 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <div className="App">
-    <BrowserRouter basename='e-comm'>
+    <BrowserRouter basename='/'>
     <Header/>
 <Routes>
 <Route element={<Hide/>} >
